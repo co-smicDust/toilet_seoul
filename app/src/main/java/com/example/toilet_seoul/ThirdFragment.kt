a3550f18d13de747585069f1782423ed5ea3fcd3
jylee102 <101354244+jylee102@users.noreply.github.com>
2022-06-12 19:14:35 +0900

    레이아웃 수정

--- app/src/main/java/com/example/toilet_seoul/ThirdFragment.kt
+++ app/src/main/java/com/example/toilet_seoul/ThirdFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.filter_search.*
 
 class ThirdFragment : Fragment(), MainActivity.onBackPressedListener {
     override fun onCreateView(
@@ -18,5 +21,24 @@ class ThirdFragment : Fragment(), MainActivity.onBackPressedListener {
     override fun onBackPressed() {
         requireActivity().supportFragmentManager.beginTransaction().remove(this).commit()
         //requireActivity().supportFragmentManager.popBackStack()
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        view?.findViewById<Button>(R.id.clear)?.setOnClickListener {
+            if (star4.isChecked) star4.isChecked = false
+            if (star3.isChecked) star3.isChecked = false
+            if (star2.isChecked) star2.isChecked = false
+            if (star_under.isChecked) star_under.isChecked = false
+            if (distance200.isChecked) distance200.isChecked = false
+            if (distance500.isChecked) distance500.isChecked = false
+            if (not_unisex.isChecked) not_unisex.isChecked = false
+            if (disabled.isChecked) disabled.isChecked = false
+            if (diaper.isChecked) diaper.isChecked = false
+            if (with_children.isChecked) with_children.isChecked = false
+            if (feminine_products.isChecked) feminine_products.isChecked = false
+            if (cctv.isChecked) cctv.isChecked = false
+        }
     }
 }
